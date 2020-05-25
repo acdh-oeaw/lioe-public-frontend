e6122f667f7242cb0f97681f25bf5ddb494cff09
Lukas Moertl <moertllukas@gmail.com>
2020-05-25 11:05:33 +0200

    adds the new acdh logo

--- src/components/App.vue
+++ src/components/App.vue
@@ -47,7 +47,7 @@
                 <v-img aspect-ratio="1" width="150" max-height="70" contain src="https://vawadioe.acdh.oeaw.ac.at/fileadmin/bilder/oeaw_logo_446x192.png" alt="" class="src" />
               </a>
               <a target="_blank" href="https://acdh.oeaw.ac.at">
-                <v-img aspect-ratio="1" width="150" max-height="70" contain src="https://vawadioe.acdh.oeaw.ac.at/fileadmin/bilder/acdh_logo120h.png" alt="" class="src" />
+                <v-img aspect-ratio="1" width="150" max-height="70" contain src="/static/img/acdh-ch-logo.png" alt="" class="src" />
               </a>
               <a target="_blank" href="https://vawadioe.acdh.oeaw.ac.at">
                 <v-img aspect-ratio="1" width="150" max-height="125" contain src="/static/img/logo-vawadioe.png" alt="" class="src" />
