2a264590dafe19e9d48d816e7840ccad39857192
tahel.singer <tahel.singer@gmail.com>
2021-11-08 18:32:42 +0100

    no more endless loading while searching

--- src/components/Database.vue
+++ src/components/Database.vue
@@ -1788,6 +1788,7 @@ export default class Database extends Vue {
       } else {
         this.totalItems = res.total.value || 0;
       }
+      this.searching = false;
     } else {
       this.init();
     }
